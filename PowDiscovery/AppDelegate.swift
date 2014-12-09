c01de910dfce0acb403d391f9890743df056c851
Attila Györffy <attila@attilagyorffy.com>
2014-12-09 23:27:33 +0100

    Initial application

--- PowDiscovery/AppDelegate.swift
+++ PowDiscovery/AppDelegate.swift
@@ -12,16 +12,27 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+        
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
+//    func applicationWillTerminate(aNotification: NSNotification) {
+//        // Insert code here to tear down your application
+//    }
 
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        
+    }
 
 }
 
