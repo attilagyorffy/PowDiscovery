2109bb1cc90ea9a3bf5ce46430ade2e5448c6b23
Attila Györffy <attila@attilagyorffy.com>
2014-12-09 22:58:40 +0100

    Initial Commit

---
+++ PowDiscovery/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PowDiscovery
+//
+//  Created by Attila Györffy on 09/12/14.
+//  Copyright (c) 2014 attilagyorffy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
